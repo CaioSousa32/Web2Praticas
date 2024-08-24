d14afb54327276f12cd981548ebfd5d568964ad8
CaioSousa32 <caio123c@gmail.com>
2024-08-23 22:04:36 -0300

    atividade 4

---
+++ Atividade 4/biblioteca/database/seeders/CategorySeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    public function run()
+    {
+        $categories = [
+            'Ficção',
+            'Não-ficção',
+            'Fantasia',
+            'Ciência',
+            'Biografia',
+            'História',
+            'Tecnologia',
+            'Arte',
+            'Culinária',
+            'Viagem'
+        ];
+
+        foreach ($categories as $category) {
+            Category::create(['name' => $category]);
+        }
+    }
+}
