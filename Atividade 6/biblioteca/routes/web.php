cf3070666ae69f7ba1d9bc4e1c3d3419f1c1dba7
CaioSousa32 <caio123c@gmail.com>
2024-08-27 23:12:15 -0300

    Concluindo atividade 6

--- Atividade 6/biblioteca/routes/web.php
+++ Atividade 6/biblioteca/routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PublisherController;
 
+// Rota para a página inicial
 Route::get('/', function () {
-    return view('welcome');
+    return view('/books');
 });
+
+// Rotas para Books
+Route::resource('books', BookController::class);
+
+// Rotas para Authors
+Route::resource('authors', AuthorController::class);
+
+// Rotas para Categories
+Route::resource('categories', CategoryController::class);
+
+// Rotas para Publishers
+Route::resource('publishers', PublisherController::class);
